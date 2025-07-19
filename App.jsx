30145c7130ef22c749e44347a8ba99f3d98a9d1f
bishnoiisubhash <bishnoisubhash778@gmail.com>
2025-07-19 09:50:31 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+const rates = {
+  "Munna fix": 9.25,
+  "Munna khula": 9.4,
+  "Taaja 500 fix": 27.3,
+  "Taaja khula": 27.5,
+  "Taaja gold fix": 34.5,
+  "Taaja gold khula": 35,
+  "Chach": 14.5,
+  "Masala chach": 9,
+  "Dahi cup 85 gm": 9.25,
+  "Dahi 1 kg": 75,
+};
